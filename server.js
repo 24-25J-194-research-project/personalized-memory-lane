e701c83c144fbecbe5f8327ce8c64b8ee650bfc0
Disira <dchanthuka2002@gmail.com>
2025-03-13 11:22:32 +0530

    chore: minor changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose"
 import cookingAssistantRoutes from "./routes/cookingAssistant.routes.js";
 
 dotenv.config();
@@ -10,11 +11,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// mongodb connection 
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 //adding routes
 app.use("/cooking-assistant", cookingAssistantRoutes);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`cooking assistant is up and running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Cooking Assistant up and running on port ${PORT} 🧑‍🍳` ));
